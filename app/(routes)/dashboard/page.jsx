a33c659ccf0b6656f9c5d5ec309532fd48b873b1
mjuradol <lopezmateojurado@gmail.com>
2025-01-03 23:25:00 -0500

    delete in dashboard path, total transfer table

--- app/(routes)/dashboard/page.jsx
+++ app/(routes)/dashboard/page.jsx
@@ -83,27 +83,10 @@ function Dashboard() {
         Que esta pasando con tu dinero, administra tus finanzas.
       </p>
 
-      <CardInfo cashList={cashList} incomeList={incomeList} />
+      <CardInfo cashList={cashList} incomeList={incomeList} /> 
       <div className="grid grid-cols-1 lg:grid-cols-3 mt-6 gap-5">
-        <div className="lg:col-span-2">
+        <div className="lg:col-span-3">
           <BarChartDashboard cashList={cashList} />
-          <ExpenseListTable
-            expensesList={expensesList}
-            refreshData={() => getCashList()}
-          />
-        </div>
-        <div className="grid gap-5">
-          <h2 className="font-bold text-lg">Latest Presupuestos</h2>
-          {cashList?.length > 0
-            ? cashList.map((cash, index) => (
-                <CashItem cash={cash} key={index} />
-              ))
-            : [1, 2, 3, 4].map((item, index) => (
-                <div
-                  className="h-[180xp] w-full
-                 bg-slate-200 rounded-lg animate-pulse"
-                ></div>
-              ))}
         </div>
       </div>
     </div>
