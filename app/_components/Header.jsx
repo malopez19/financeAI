b013d6c4fcf08db9778265b3599193152ddfd041
mjuradol <lopezmateojurado@gmail.com>
2025-01-05 21:24:52 -0500

    Update Header and Hero components with new styles and logo; translate button texts to Spanish

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -7,10 +7,10 @@ import Link from "next/link";
 function Header() {
   const { user, isSignedIn } = useUser();
   return (
-    <div className="p-5 flex justify-between items-center shadow-sm bg-blue-800">
+    <div className="p-5 flex justify-between items-center shadow-sm bg-blue-900">
       <div className="flex flex-row items-center">
-        <Image src={"/chart-donut.svg"} alt="logo" width={40} height={25} />
-        <span className="text-indigo-100  font-bold text-xl">FinappIA</span>
+        <Image src={"/logo.svg"} alt="logo" width={40} height={25} />
+        <span className="text-neutral-50 text-opacity-100 font-bold text-xl ml-2">FinappIA</span>
       </div>
       {isSignedIn ? (
         <UserButton />
@@ -18,11 +18,11 @@ function Header() {
         <div className="flex gap-3  items-center ">
           <Link href={"/dashboard"}>
             <Button variant="outline" className="rounded-full">
-              Dashboard
+              Ir al Panel
             </Button>
           </Link>
           <Link href={"/sign-in"}>
-            <Button className="rounded-full">Get Started</Button>
+            <Button className="bg-amber-500 hover:bg-amber-400 rounded-full">Empezar</Button>
           </Link>
         </div>
       )}
--- app/_components/Hero.jsx
+++ app/_components/Hero.jsx
@@ -4,14 +4,14 @@ import { ContainerScroll } from "../../components/ui/container-scroll-animation"
 
 function Hero() {
   return (
-    <section className="bg-blue-800 flex items-center flex-col">
+    <section className="bg-blue-900 flex items-center flex-col">
       <div className="flex flex-col overflow-hidden">
         <ContainerScroll
           titleComponent={
             <>
               <h1 className="text-4xl font-semibold text-black dark:text-white mb-8">
                 Administra Tu Dinero Personal <br />
-                <span className="text-4xl md:text-[6rem] text-indigo-100 font-bold mt-1 mb-24 leading-none">
+                <span className="text-4xl md:text-[6rem] text-neutral-50 text-opacity-100 font-bold mt-1 mb-24 leading-none">
                   App Financiera
                 </span>
               </h1>
@@ -20,7 +20,7 @@ function Hero() {
         >
           <Image
             src={`/dashboard.png`}
-            alt="hero"
+            alt="imagen dashboard"
             height={720}
             width={1400}
             className="mx-auto rounded-2xl object-cover h-full object-left-top"
