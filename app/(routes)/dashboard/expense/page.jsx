15e108b4e11c37d3d739850355ec2c484826d7ac
mjuradol <lopezmateojurado@gmail.com>
2025-01-19 21:30:09 -0500

    Remove CreateIncomes, IncomeList, and TotalIncome components: streamline income management by eliminating unused components and updating the Income page title

--- app/(routes)/dashboard/expense/page.jsx
+++ app/(routes)/dashboard/expense/page.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import IncomeList from "./_components/IncomeList";
 
 function Income() {
   return (
     <div className="p-10">
-      <h2 className="font-bold text-3xl">My Income Streams</h2>
-      <IncomeList />
+      <h2 className="font-bold text-3xl">Transferir</h2>
     </div>
   );
 }
