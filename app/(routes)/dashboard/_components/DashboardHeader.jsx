22fb615c802a48b4b6f9153db6e37b879f5e866d
mjuradol <lopezmateojurado@gmail.com>
2025-02-01 02:55:29 -0500

    Update DashboardHeader component; implement user sign-out logic based on authentication state

--- app/(routes)/dashboard/_components/DashboardHeader.jsx
+++ app/(routes)/dashboard/_components/DashboardHeader.jsx
@@ -1,18 +1,26 @@
-import { UserButton } from '@clerk/nextjs'
-import React from 'react'
+import { UserButton, useUser, useClerk } from '@clerk/nextjs';
+import React, { useEffect } from 'react';
 
 function DashboardHeader() {
+  const { user, isLoaded } = useUser(); // Añade isLoaded para verificar si el estado del usuario está cargado
+  const { signOut } = useClerk();
+
+  useEffect(() => {
+    if (isLoaded && !user) { // Verifica si el estado del usuario está cargado y si no hay usuario
+      signOut();
+    }
+  }, [user, isLoaded, signOut]);
+
   return (
     <div className='p-5 shadow-sm border-b flex justify-between'>
-        <div>
-          
-        </div>
-        <div>
-            <UserButton fallbackRedirectUrl='/sign-in' afterSignOutUrl='/'/>
-        </div>
-       
+      <div>
+        {/* Puedes agregar contenido adicional aquí si es necesario */}
+      </div>
+      <div>
+        <UserButton fallbackRedirectUrl='/sign-in' afterSignOutUrl='/' />
+      </div>
     </div>
-  )
+  );
 }
 
-export default DashboardHeader
+export default DashboardHeader;
