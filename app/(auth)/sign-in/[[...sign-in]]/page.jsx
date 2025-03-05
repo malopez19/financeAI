40e52bc472f8b4ca80c092bdd4e3c94d85361a6c
malopez19 <lopezmateojurado@gmail.com>
2025-03-04 20:09:55 -0500

    Update authentication pages; adjust layout for improved responsiveness and change branding from 'Finapp' to 'Qoin'

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -2,16 +2,12 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <section className="bg-[#27445d]">
+    <section className="min-h-screen bg-[#27445d]">
       <div className="lg:grid lg:min-h-screen ">
-        <section className="relative flex h-32 sm:hidden bg-[#27445d] lg:col-span-5 lg:h-full xl:col-span-6 justify-center items-center">
-          
-        </section>
-
-        <main className="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6">
-          <div className="max-w-xl lg:max-w-3xl">
-            <div className="relative -mt-16 lg:hidden flex justify-center">
-              <h1 className="my-10 text-2xl font-bold text-white sm:text-3xl md:text-4xl">
+        <main className="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-10 xl:col-span-6">
+          <div className="max-w-xl lg:max-w-3xl mt-1">
+            <div className="flex justify-center">
+              <h1 className="my-10 text-4xl font-bold text-white sm:text-3xl md:text-4xl">
                 Bienvenido a Qoin 
               </h1>
             </div>
--- app/(auth)/sign-up/[[...sign-up]]/page.jsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.jsx
@@ -2,12 +2,12 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <section className="bg-[#27445d]" role="region">
+    <section className="min-h-screen bg-[#27445d]" role="region">
       <div className="sm:grid sm:min-h-screen sm:grid-cols-1">
         <main className="flex items-center justify-center px-8 py-8 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6">
-          <div className="max-w-xl lg:max-w-3xl">
-            <div className="relative -mt-16 block lg:hidden">
-              <h1 className="mt-12 text-2xl font-bold text-white sm:text-3xl flex justify-center md:text-4xl">
+          <div className="max-w-xl lg:max-w-3xl mt-14">
+            <div className="flex justify-center">
+              <h1 className="mb-12 text-4xl font-bold text-white sm:text-3xl flex justify-center align-middle md:text-4xl">
                 Registrate a Qoin 
               </h1>
             </div>
